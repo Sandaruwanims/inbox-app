08fbfe6a04a4ca094c701ddd354e4385867a3543
sandaruwanims <sandaruwanims@gmail.com>
2022-07-28 00:07:11 +0530

    Source package changed

--- src/main/java/io/mail/InboxApp.java
+++ src/main/java/io/mail/InboxApp.java
@@ -1,4 +1,4 @@
-package io.javabrains;
+package io.mail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/io/mail/SecurityAdapter.java
+++ src/main/java/io/mail/SecurityAdapter.java
@@ -1,4 +1,4 @@
-package io.javabrains;
+package io.mail;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
